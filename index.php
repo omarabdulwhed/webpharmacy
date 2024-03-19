f1891780374cb113137ba294573a1be50db639b5
omarabdulwhed <113902091+omarabdulwhed@users.noreply.github.com>
2024-03-19 22:39:31 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Parmicy</title>
+   <link rel="stylesheet" href="pharmacy.css">
+
+   <!--bootsrap css -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+       integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
+       <!--font awesome link-->
+
+     <link rel="stylesheet"
+   href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" 
+  integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+   crossorigin="anonymous" 
+   referrerpolicy="no-referrer" />
+  
+   </head>
+<body>
+   <!--bootsrap js -->
+
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+       integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+       crossorigin="anonymous"></script>
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+       integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" />
+  
+  <div>
+
+  
+  
+     <!--Nav-->
+
+       <div class="container-fluid">
+                 <!--first child-->
+                 <nav class="navbar navbar-expand-lg bg-body-tertiary">
+ <div class="container-fluid">
+   <img src="omar.png"  alt=" "class="logo">
+   <a class="navbar-brand" href="#">Navbar</a>
+   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+     <span class="navbar-toggler-icon"></span>
+   </button>
+   <div class="collapse navbar-collapse" id="navbarSupportedContent">
+     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+       <li class="nav-item">
+         <a class="nav-link active" aria-current="page" href="#">Home</a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link" href="#">Registre</a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link" href="#">Contact</a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link" href="#">Cart</a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link" href="#">Total price</a>
+       </li>
+       
+       <li class="nav-item">
+         <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+       </li>
+     </ul>
+     <form class="d-flex" role="search">
+       <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+       <button class="btn btn-outline-success" type="submit">Search</button>
+     </form>
+   </div>
+ </div>
+</nav>
+
+    <!--  <div class="bg-info p-3 text-center">
+       <p>all rigth reserved Designed by Mohamd-2024</p>
+    </div>
+</form>
+           </div>
+       </div>
+   </nav> -->
+
+
+ <!-- 2child-->
+<nav class="navba1">
+  <ul class="navbar-nav me-auto " >
+    <li class="nav-item">
+      <a class="nav-link"  href="#">Welcome ya marid</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Login</a>
+    </li>
+  </ul>
+</nav>
+ <!-- 3child-->
+
+<div class="bg-ligth">
+    <h3 class="text-center">Hidden Store</h3>
+    <p class="text-center">Communication between user and pharmacy</p>
+</div>
+ <!-- 4child-->
+ <div class="row">
+<div class="col-md-10">
+<!--product-->
+    
+<div class="row">
+    <div class="col-md-4 mb-2"><div class="card" style="width: 18rem;">
+  <img src="omar.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Add To cart</a>
+    <a href="#" class="btn btn-secondary">view more</a>
+  </div>
+</div></div>
+    <div class="col-md-4 mb-2"><div class="card" style="width: 18rem;">
+  <img src="Panadol_2_730x.progressive.webp" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Add To cart</a>
+    <a href="#" class="btn btn-secondary">view more</a>
+
+  </div>
+</div></div>
+    <div class="col-md-4 mb-2"><div class="card" style="width: 18rem;">
+  <img src="medical-banner-with-doctor-wearing-goggles_23-2149611193.avif" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Add To cart</a>
+    <a href="#" class="btn btn-secondary">view more</a>
+
+  </div>
+</div></div>
+<div class="col-md-4 mb-2"><div class="card" style="width: 18rem;">
+  <img src="Panadol_2_730x.progressive.webp" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Add To cart</a>
+    <a href="#" class="btn btn-secondary">view more</a>
+
+  </div>
+</div></div>
+<div class="col-md-4 mb-2"><div class="card" style="width: 18rem;">
+  <img src="Panadol_2_730x.progressive.webp" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Add To cart</a>
+    <a href="#" class="btn btn-secondary">view more</a>
+
+  </div>
+</div></div>
+<div class="col-md-4 mb-2"><div class="card" style="width: 18rem;">
+  <img src="Panadol_2_730x.progressive.webp" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Add To cart</a>
+    <a href="#" class="btn btn-secondary">view more</a>
+
+  </div>
+</div>
+<div class="col-md-2 bg-secondary p-0">
+<ul class="navbar-nav me-auto center">
+    <li class="nav-item bg-info">
+<a href="#" class="nav-link text-ligth"><h4>Delivery Brands</h4></a>
+</li>
+<li class="nav-item bg-info">
+<a href="#" class="nav-link text-ligth"><h4>Brand1</h4></a>
+</li>
+<li class="nav-item bg-info">
+<a href="#" class="nav-link text-ligth"><h4> Brands2</h4></a>
+</li>
+<li class="nav-item bg-info">
+<a href="#" class="nav-link text-ligth"><h4>Brand3</h4></a>
+</li>
+<li class="nav-item bg-info">
+<a href="#" class="nav-link text-ligth"><h4> Brand4</h4></a>
+</li>
+<li class="nav-item bg-info">
+<a href="#" class="nav-link text-ligth"><h4>Brand5</h4></a>
+</li>
+</div>
+</div>
+
+
+</div>
+
+
+    <ul class="nabar-nav me-auto">
+
+    </ul>
+   </div>
+      <div class="bg- p-3 text-center">
+       <p>all rigth reserved Designed by Mohamd-2024</p>
+    </div>
+</form>
+           </div>
+       </div>
+   </nav> 
+ 
+</div>
+</body>
+</html>
